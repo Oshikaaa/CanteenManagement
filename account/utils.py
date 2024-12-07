e1dd10a703aa5b5a6f7c4c4db7ae82b54bd427c1
Oshikaaa <Oshika.thamsuhang@aitm.edu.np>
2024-12-08 00:22:50 +0545

    CRUD for food items

--- account/utils.py
+++ account/utils.py
@@ -1,9 +1,9 @@
 def detectUser(user):
     if user.role == 1:
-        redirectUrl = 'index'
+        redirectUrl = 'adminDashboard'
         return redirectUrl
     elif user.role == 2:
-        redirectUrl = 'index'
+        redirectUrl = 'custDashboard'
         return redirectUrl
     elif user.role == None and user.is_superadmin:
         redirectUrl = '/admin'
--- food/urls.py
+++ food/urls.py
@@ -7,6 +7,15 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/' , views.checkout,name='checkout'),
     path('get_cart_details/', views.get_cart_details, name='get_cart_details'),
+    
+    path('add_food/', views.add_food, name='add_food'),
+    path('edit_product/<int:pk>/', views.edit_product, name='edit_product'),
+    
+    path('product_builder/', views.product_builder, name='product_builder'),
+    path('product_category/<int:pk>/', views.productItem_by_category, name='product_category'),
+    path('product_detail/<int:id>/' , views.product_detail,name='product_detail'),
+
+
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('decrease_cart/<int:product_id>/', views.decrease_cart, name='decrease_cart'),
     path('delete_cart/<int:cart_id>/', views.delete_cart, name='delete_cart'),  # Delete cart item
