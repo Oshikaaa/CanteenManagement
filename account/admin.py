0a33cc9389546e07684c5eb904da6f4bbeb45229
Oshikaaa <Oshika.thamsuhang@aitm.edu.np>
2024-12-14 00:30:27 +0545

    recommendation system working on notebook kernel

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-
 from account.models import User, UserProfile
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(UserProfile)
+# Customize User admin panel
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active', 'is_staff', 'is_admin')
+    list_filter = ('role', 'is_active', 'is_staff', 'is_admin')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+    readonly_fields = ('date_joined', 'last_login')
+
+# Customize UserProfile admin panel
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'address', 'created_at', 'modified_at')
+    search_fields = ('user__email', 'user__username', 'city')
+    ordering = ('user',)
+
+# Register models
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
